63337c1b3eec83f1dedadc75775e4518265d00a3
longc-chen <longc-chen@users.noreply.github.com>
2017-10-21 11:28:40 +0800

    Initial Commit

---
+++ StringSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StringSample
+//
+//  Created by LongCh on 2017/10/21.
+//  Copyright © 2017年 LongCh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
